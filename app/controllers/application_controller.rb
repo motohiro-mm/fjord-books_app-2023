8c24d6eaa7806677308691843845148ebfe4d16d
motohiro-mm <polarbear.dx@gmail.com>
2023-04-13 16:34:28 +0900

    switch_localeメソッドをprivateメソッドへ修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,14 @@
 class ApplicationController < ActionController::Base
   around_action :switch_locale
 
+  def success(model_name)
+    t 'activerecord.notice.success', deep_interpolation: true, model: model_name
+  end
+
+  private
+
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
     I18n.with_locale(locale, &action)
-  end
-
-  def success(model_name)
-    t 'activerecord.notice.success', deep_interpolation: true, model: model_name
   end
 end
