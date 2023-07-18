80876345e4f21a9c3fae8a3e7e42b7fe8a740c0b
motohiro-mm <polarbear.dx@gmail.com>
2023-07-18 21:41:40 +0900

    1ページに表示する数をそれぞれ設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.order(:id).page(params[:page])
+    @users = User.order(:id).page(params[:page]).per(8)
   end
 
   # GET /users/1 or /users/1.json
