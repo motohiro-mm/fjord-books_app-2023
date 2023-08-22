2946dd39a6b466f66db014a827a16238a4f4dd1a
motohiro-mm <polarbear.dx@gmail.com>
2023-08-22 20:28:27 +0900

    コメント機能を実装、ユーザーの名前表示を修正

---
+++ app/controllers/books/comments_controller.rb
@@ -0,0 +1,43 @@
+class Books::CommentsController < ApplicationController
+  before_action :set_commentable, only: %i[create update edit destroy]
+  before_action :set_comment, only: %i[edit update destroy]
+  
+  def edit; end
+
+  def create
+    @comment = @commentable.comments.new(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_to url_for(@commentable), notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    else
+      render "books/show", status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to url_for(@commentable), notice: t('controllers.common.notice_update', name: Comment.model_name.human)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to url_for(@commentable), notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+  end
+
+  private
+
+  def set_commentable
+    @commentable = Book.find(params[:book_id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
