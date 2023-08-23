2cfe33521f1af562f1385f63ef7efbed3bae65b9
motohiro-mm <polarbear.dx@gmail.com>
2023-08-23 19:10:07 +0900

    コントローラをコメントコントローラを親にして３つにした、ビューを１つにまとめた

--- app/controllers/books/comments_controller.rb
+++ app/controllers/books/comments_controller.rb
@@ -1,32 +1,7 @@
 # frozen_string_literal: true
 
-class Books::CommentsController < ApplicationController
+class Books::CommentsController < CommentsController
   before_action :set_commentable, only: %i[create update edit destroy]
-  before_action :set_comment, only: %i[edit update destroy]
-
-  def edit; end
-
-  def create
-    @comment = @commentable.comments.new(comment_params)
-    if @comment.save
-      redirect_to url_for(@commentable), notice: t('controllers.common.notice_create', name: Comment.model_name.human)
-    else
-      render 'books/show', status: :unprocessable_entity
-    end
-  end
-
-  def update
-    if @comment.update(comment_params)
-      redirect_to url_for(@commentable), notice: t('controllers.common.notice_update', name: Comment.model_name.human)
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @comment.destroy
-    redirect_to url_for(@commentable), notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
-  end
 
   private
 
@@ -34,11 +9,8 @@ class Books::CommentsController < ApplicationController
     @commentable = Book.find(params[:book_id])
   end
 
-  def set_comment
-    @comment = Comment.find(params[:id])
-  end
-
-  def comment_params
-    params.require(:comment).permit(:content).merge(user_id: current_user.id)
+  def render_commentable_show
+    @book = @commentable
+    render 'books/show', status: :unprocessable_entity
   end
 end
--- app/controllers/reports/comments_controller.rb
+++ app/controllers/reports/comments_controller.rb
@@ -1,32 +1,7 @@
 # frozen_string_literal: true
 
-class Reports::CommentsController < ApplicationController
+class Reports::CommentsController < CommentsController
   before_action :set_commentable, only: %i[create update edit destroy]
-  before_action :set_comment, only: %i[edit update destroy]
-
-  def edit; end
-
-  def create
-    @comment = @commentable.comments.new(comment_params)
-    if @comment.save
-      redirect_to url_for(@commentable), notice: t('controllers.common.notice_create', name: Comment.model_name.human)
-    else
-      render 'reports/show', status: :unprocessable_entity
-    end
-  end
-
-  def update
-    if @comment.update(comment_params)
-      redirect_to url_for(@commentable), notice: t('controllers.common.notice_update', name: Comment.model_name.human)
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @comment.destroy
-    redirect_to url_for(@commentable), notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
-  end
 
   private
 
@@ -34,11 +9,8 @@ class Reports::CommentsController < ApplicationController
     @commentable = Report.find(params[:report_id])
   end
 
-  def set_comment
-    @comment = Comment.find(params[:id])
-  end
-
-  def comment_params
-    params.require(:comment).permit(:content).merge(user_id: current_user.id)
+  def render_commentable_show
+    @report = @commentable
+    render 'reports/show', status: :unprocessable_entity
   end
 end
