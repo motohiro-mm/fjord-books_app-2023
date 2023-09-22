42cb734478fde0d4b41dcba267bf9fa51a80c755
motohiro-mm <polarbear.dx@gmail.com>
2023-09-22 12:39:49 +0900

    reportモデルとmention_relationモデルに多対多、自己結合を設定

--- app/models/mention_relation.rb
+++ app/models/mention_relation.rb
@@ -1,2 +1,4 @@
 class MentionRelation < ApplicationRecord
+  belongs_to :mentioning_report, class_name: "Report", optional: true
+  belongs_to :mentioned_report, class_name: "Report"
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -4,6 +4,11 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
+  has_many :mentioning_relations, foreign_key: "mentioning_report_id", class_name: "MentionRelation", dependent: :destroy
+  has_many :mentioning_reports, through: :mentioning_relations, source: :mentioned_report
+  has_many :mentioned_relations, foreign_key: "mentioned_report_id", class_name: "MentionRelation", dependent: :destroy
+  has_many :mentioned_reports, through: :mentioned_relations, source: :mentioning_report
+
   validates :title, presence: true
   validates :content, presence: true
 
