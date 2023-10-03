2358968017368245db1ec0ab2428c7918925815e
motohiro-mm <polarbear.dx@gmail.com>
2023-10-03 21:28:17 +0900

    optional: trueを削除

--- app/models/mention_relation.rb
+++ app/models/mention_relation.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class MentionRelation < ApplicationRecord
-  belongs_to :mentioning_report, class_name: 'Report', optional: true
+  belongs_to :mentioning_report, class_name: 'Report'
   belongs_to :mentioned_report, class_name: 'Report'
 
   validates :mentioning_report, presence: true, uniqueness: { scope: :mentioned_report }
