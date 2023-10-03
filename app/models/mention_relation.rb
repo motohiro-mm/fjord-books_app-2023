b7bd8c3750b3776ce3fff71afc2aa94957d4e414
motohiro-mm <polarbear.dx@gmail.com>
2023-10-03 21:20:37 +0900

    mention_relationモデルにuniquenessを追加

--- app/models/mention_relation.rb
+++ app/models/mention_relation.rb
@@ -4,6 +4,6 @@ class MentionRelation < ApplicationRecord
   belongs_to :mentioning_report, class_name: 'Report', optional: true
   belongs_to :mentioned_report, class_name: 'Report'
 
-  validates :mentioning_report, presence: true
+  validates :mentioning_report, presence: true, uniqueness: { scope: :mentioned_report }
   validates :mentioned_report, presence: true
 end
