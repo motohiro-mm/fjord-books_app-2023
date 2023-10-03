2e734384df5e9e56801a909ea4bbb3060f04140c
motohiro-mm <polarbear.dx@gmail.com>
2023-10-03 20:32:04 +0900

    mentioning_relationsをmentionings、mentioned_relationsをmentionedsに変更

--- app/models/report.rb
+++ app/models/report.rb
@@ -7,10 +7,10 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  has_many :mentioning_relations, foreign_key: 'mentioning_report_id', class_name: 'MentionRelation', inverse_of: :mentioning_report, dependent: :destroy
-  has_many :mentioning_reports, through: :mentioning_relations, source: :mentioned_report
-  has_many :mentioned_relations, foreign_key: 'mentioned_report_id', class_name: 'MentionRelation', inverse_of: :mentioned_report, dependent: :destroy
-  has_many :mentioned_reports, through: :mentioned_relations, source: :mentioning_report
+  has_many :mentionings, foreign_key: 'mentioning_report_id', class_name: 'MentionRelation', inverse_of: :mentioning_report, dependent: :destroy
+  has_many :mentioning_reports, through: :mentionings, source: :mentioned_report
+  has_many :mentioneds, foreign_key: 'mentioned_report_id', class_name: 'MentionRelation', inverse_of: :mentioned_report, dependent: :destroy
+  has_many :mentioned_reports, through: :mentioneds, source: :mentioning_report
 
   validates :title, presence: true
   validates :content, presence: true
@@ -24,11 +24,11 @@ class Report < ApplicationRecord
   end
 
   def create_mention
-    report_mentioning_relations =
+    report_mentionings =
       ids_in_content(self).map do |id|
-        mentioning_relations.build(mentioned_report_id: id)
+        mentionings.build(mentioned_report_id: id)
       end
-    report_mentioning_relations.each(&:save)
+    report_mentionings.each(&:save)
   end
 
   def update_mention
@@ -36,24 +36,24 @@ class Report < ApplicationRecord
     new_mentioned_report_ids = ids_in_content(self).map(&:to_i)
 
     add_mentioned_report_ids = new_mentioned_report_ids - old_mentioned_reports_ids
-    add_mentioning_relations(add_mentioned_report_ids, mentioning_relations)
+    add_mentionings(add_mentioned_report_ids, mentionings)
 
     delete_mentioned_report_ids = old_mentioned_reports_ids - new_mentioned_report_ids
-    delete_mentioning_relations(delete_mentioned_report_ids, mentioning_relations)
+    delete_mentionings(delete_mentioned_report_ids, mentionings)
   end
 
   def ids_in_content(report)
     report.content.scan(%r{http://localhost:3000/reports/(\d+)}).flatten.uniq
   end
 
-  def add_mentioning_relations(add_mentioned_report_ids, report_mentioning_relations)
-    add_mentioned_report_ids.each { |add_mentioned_report_id| report_mentioning_relations.create(mentioned_report_id: add_mentioned_report_id) }
+  def add_mentionings(add_mentioned_report_ids, report_mentionings)
+    add_mentioned_report_ids.each { |add_mentioned_report_id| report_mentionings.create(mentioned_report_id: add_mentioned_report_id) }
   end
 
-  def delete_mentioning_relations(delete_mentioned_report_ids, report_mentioning_relations)
+  def delete_mentionings(delete_mentioned_report_ids, report_mentionings)
     delete_mentioned_report_ids.each do |delete_mentioned_report_id|
-      delete_mentioning_relation = report_mentioning_relations.find_by(mentioned_report_id: delete_mentioned_report_id)
-      delete_mentioning_relation.destroy
+      delete_mentioning = report_mentionings.find_by(mentioned_report_id: delete_mentioned_report_id)
+      delete_mentioning.destroy
     end
   end
 end
